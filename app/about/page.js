258f38f6372eaa0f3b7a96efb96cd0a2e91a36f6
Purnima <poudelpurnima445@gmail.com>
2025-11-11 21:19:10 +0545

    About page third section

--- app/about/page.js
+++ app/about/page.js
@@ -4,11 +4,6 @@ const page = () => {
   return (
     <div id="main">
     <section className="topsec">
-        <div className="custom-shape-divider-bottom-1762686975">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-            </svg> 
-        </div>
     <div className="sec1">
         <div className="emoji">
             <i className="fa-solid fa-building-columns"></i>
@@ -33,14 +28,15 @@ const page = () => {
             <div className="shape">Excellence and Perseverance</div>
             <div className="shape">Community and Global Citizenship</div>
           </div>
+         
           
     </section>
     <section className="thirdsec">
         <h1>MEET OUR TEACHERS</h1>
         <div className="pic">
-            <div className="circle"></div>
-            <div className="circle"></div>
-            <div className="circle"></div>
+            <div className="circle circle1"></div>
+            <div className="circle circle2"></div>
+            <div className="circle circle3"></div>
         </div>
     </section>
     <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/gsap.min.js"></Script>
